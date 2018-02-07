ecbbf930b5c755ccfef34f02400f87ebc260643b
leon1757tw <leon1757tw@gmail.com>
2018-02-08 02:49:49 +0800

    fixed stupid bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+//'use strict';
 
 //const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const PAGE_ACCESS_TOKEN = EAAH7yKeipFkBAJQMADJ2M7rrwSyXIcFTG3HioVYvSOUlVTxzuDXv1iu9iafkFCi1u7KnuPsS8erg7UeTdY83TMptZBaebkeF22I2xT59CdkW0kWA0WSNMTxDUNrtvAnSX7FU7cycZCJLWSZAqvuzqA2GZBlwba2RjrZBHZAcl9WwZDZD
+const PAGE_ACCESS_TOKEN = 'EAAH7yKeipFkBAJQMADJ2M7rrwSyXIcFTG3HioVYvSOUlVTxzuDXv1iu9iafkFCi1u7KnuPsS8erg7UeTdY83TMptZBaebkeF22I2xT59CdkW0kWA0WSNMTxDUNrtvAnSX7FU7cycZCJLWSZAqvuzqA2GZBlwba2RjrZBHZAcl9WwZDZD'
 // Imports dependencies and set up http server
 const
     request = require('request'),

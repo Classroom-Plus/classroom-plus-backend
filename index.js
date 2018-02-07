10956c329f817dd9fd260ed423b5339ef06f9468
leon1757tw <leon1757tw@gmail.com>
2018-02-07 22:56:15 +0800

    setting heroku

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require('express')
+const app = express();
 
+app.get('/', function(req, res){
+    res.send('hello world!')
+})
+
+app.listen(443, function(){
+    console.log('server start!')
+})

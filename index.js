22337e26f180769e894ee7cab60f5d0565744f26
leon1757tw <leon1757tw@gmail.com>
2018-03-06 15:34:56 +0800

    modified for test

--- index.js
+++ index.js
@@ -1,133 +1,19 @@
-const express = require('express')
-const request = require('request')
-const body_parser = require('body-parser')
-const env = require('dotenv').config()
-const app = express()
+const webhooks = require('./routers/webhooks');
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+const express = require('express');
+const body_parser = require('body-parser');
 
-app.use(body_parser.json())
+const app = express();
 
-app.listen(process.env.PORT || 3000, () => console.log('server start! webhook is listening'))
+app.use(body_parser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/webhook', (req, res) => {
-    let body = req.body
+app.use('/webhook', webhooks);
 
-    if (body.object === 'page') {
-        body.entry.forEach(function (entry) {
-            let webhook_event = entry.messaging[0]
-            let sender_psid = webhook_event.sender.id
-            console.log(webhook_event)
-            console.log('Sender ID: ' + sender_psid)
-
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message)
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback)
-            }
-        })
-        res.status(200).send('EVENT_RECEIVED')
-    } else {
-        res.sendStatus(404)
-    }
+app.get('/', (req, res) => {
+    res.send('Hello world');
 })
 
-app.get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = "leon1757tw"
-    let mode = req.query['hub.mode']
-    let token = req.query['hub.verify_token']
-    let challenge = req.query['hub.challenge']
-
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED')
-            res.status(200).send(challenge)
-        } else {
-            res.sendStatus(403)
-        }
-    }
-})
-
-function handleMessage(sender_psid, received_message) {
-    let response;
-
-    // Checks if the message contains text
-    if (received_message.text) {
-        // Create the payload for a basic text message, which
-        // will be added to the body of our request to the Send API
-        response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-        }
-    } else if (received_message.attachments) {
-        // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
-    }
-
-    // Send the response message
-    callSendAPI(sender_psid, response);
-}
-
-function handlePostback(sender_psid, received_postback) {
-    console.log('ok')
-    let response;
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
-}
-
-function callSendAPI(sender_psid, response) {
-    // Construct the message body
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
-
-    // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
-}
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server start! webhook is listening');
+});
---
+++ routers/webhooks.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const env = require('dotenv').config();
+
+router.get('/', (req, res) => {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+router.post('/', (req, res) => {
+    const data = req.body;
+
+    if (data.object === 'page') {
+        data.entry.forEach((entry) => {
+            entry.messaging.forEach((event) => {
+                if (event.message) {
+                    handleMessage(event.sender.id, event.message);
+                } else if (event.postback) {
+                    // add message handler here                    
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Send the response message
+    callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+module.exports = router;

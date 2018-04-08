a95f991924ef67a92570e3d1ed2edbf38e748e8a
LIN YI-FENG <leon1757tw@gmail.com>
2018-04-08 19:58:22 +0800

    add pm2 deploy file

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'classroom-plus-backend',
+      script: 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'leon1757tw',
+      host: '35.229.133.151',
+      ref: 'origin/master',
+      repo: 'git@github.com:leon1757tw/classroom-plus-backend.git',
+      path: '/home/leon1757tw/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+  }
+};
